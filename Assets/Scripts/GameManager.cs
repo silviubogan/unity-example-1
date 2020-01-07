4b1647a16232de8b88cf9c10e3a33df6d4767c3f
Silviu Bogan <silviubogan@gmail.com>
2020-01-07 11:54:42 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject whiteCellPrefab;
+
+    int N = 30, M = 20, size = 16;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform p = GameObject.Find("Canvas").transform;
+
+        for (int i = 1; i <= N; ++i)
+        {
+            for (int j = 1; j <= M; ++j)
+            {
+                GameObject g = Instantiate(whiteCellPrefab, p);
+                g.transform.localPosition = new Vector3(i * size, j * size);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
